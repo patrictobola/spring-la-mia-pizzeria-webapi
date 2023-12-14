6cdf849c5dc0da6046f8be2452049192b72b4086
patrictobola <tobola92@gmail.com>
2023-12-14 14:39:35 +0100

    done a raw index and tested api calls

---
+++ front-end/src/components/index.vue
@@ -0,0 +1,43 @@
+<script>
+import { ref } from 'vue'
+export default {
+  data() {
+    return {
+      pizzas: [],
+    };
+  },
+  mounted() {
+    this.fetchPizzas();
+  },
+  methods: {
+    async fetchPizzas() {
+      try {
+        const response = await fetch('http://localhost:8080/api/pizzas');
+        const pizzas = await response.json();
+        this.pizzas = pizzas;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+
+// defineProps({
+//   msg: String,
+
+// })
+</script>
+
+<template>
+  <!-- <h1>{{ msg }}</h1> -->
+  <div>
+    <h2>List of all pizzas:</h2>
+    <ul>
+      <li v-for="pizza in pizzas" :key="pizza.id">
+        {{ pizza.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
--- src/main/java/org/java/spring/auth/conf/AuthConf.java
+++ src/main/java/org/java/spring/auth/conf/AuthConf.java
@@ -1,14 +1,20 @@
 package org.java.spring.auth.conf;
 
 import org.java.spring.auth.db.serv.UserService;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class AuthConf {
@@ -48,5 +54,33 @@ public class AuthConf {
 
 		return authProvider;
 	}
+	@Bean
+    FilterRegistrationBean<CorsFilter> getCorsSettings() {
+		
+        final CorsConfiguration config = new CorsConfiguration();
+              
+        config.addAllowedOrigin("http://localhost:5173");
+        
+        // HEADERS
+        config.addAllowedHeader("Content-Type");
+        config.addAllowedHeader("Authorization");
+        config.addAllowedHeader("X-XSRF-TOKEN");
+        config.addAllowedHeader("Accept");
+        
+        // METHODS
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.POST);
+		config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        
+        // SET CONFIG ON PATHS
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        
+        final FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        
+        return bean;
+    }
 
 }
